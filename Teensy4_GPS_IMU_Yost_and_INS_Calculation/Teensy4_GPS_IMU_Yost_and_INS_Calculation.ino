ecfe1dea74bda984c645c5f8922bedd953dc76fa
Satria Wijayandaru <satriawijayandaru@gmail.com>
2023-02-14 10:26:26 +0700

    working calculation, but not accurate yet

--- Teensy4_GPS_IMU_Yost_and_INS_Calculation/Teensy4_GPS_IMU_Yost_and_INS_Calculation.ino
+++ Teensy4_GPS_IMU_Yost_and_INS_Calculation/Teensy4_GPS_IMU_Yost_and_INS_Calculation.ino
@@ -1,9 +1,129 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <TinyGPS++.h>
 
+#define ser1 Serial
+#define GPS Serial2
+#define IMU Serial3
+
+double dt;
+unsigned long previousMillis;
+
+//IMU
+String incomingCSV[20];
+String incomingString;
+int currentCSVIndex = 0;
+int maxCSVIndex;
+char incomingByte;
+int incomingInt;
+unsigned long previousMillisIMU = 0;
+float imuX, imuY, imuZ;
+
+//INS Calculation
+float gx, gy, gz;
+float x, y, z;
+float k = 0.98;
+
+//GPS
+float lat, lon, alt;
+
+void setup() {
+  GPS.begin(115200);
+  IMU.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  getImu(100);
+  
+}
 
+void insCalc() {
+  unsigned long currentMillis = millis();
+  dt = (currentMillis - previousMillis) / 1000.0;
+  
+  gx = imuX * 9.8;
+  gy = imuY * 9.8;
+  gz = imuZ * 9.8;
+
+  x = k * (x + gx * dt) + (1 - k) * lat;
+  y = k * (y + gy * dt) + (1 - k) * lon;
+  z = k * (z + gz * dt) + (1 - k) * alt;
+  ser1.print("DT = ");
+  ser1.println(dt);
+  ser1.print("lat INS = ");
+  ser1.println(x);
+  ser1.print("lng INS = ");
+  ser1.println(y);
+  ser1.print("alt INS = ");
+  ser1.println(z);
+  previousMillis = currentMillis;
+}
+
+void getImu(int interval) {
+  if (IMU.available()) {
+    serialIN(IMU.read(), 0);
+  }
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillisIMU >= interval) {
+    previousMillisIMU = currentMillis;
+    ser1.print(millis());
+    ser1.print("--");
+    ser1.println("Requesting :34");
+    IMU.println(":34");
+  }
+}
+
+void dataPreparation() {
+  imuX   = incomingCSV[2].toFloat();
+  imuY   = incomingCSV[0].toFloat();
+  imuZ   = incomingCSV[1].toFloat();
+//  imuX   = radToDeg(incomingCSV[2].toFloat());
+//  imuY   = radToDeg(incomingCSV[0].toFloat());
+//  imuZ   = radToDeg(incomingCSV[1].toFloat());
+
+  Serial.print("X = ");
+  Serial.println(imuX);
+  Serial.print("Y = ");
+  Serial.println(imuY);
+  Serial.print("Z = ");
+  Serial.println(imuZ);
+  insCalc();
+}
+
+float radToDeg(float data) {
+  return data * (180 / PI);
+}
+
+void serialIN(char incomingSerial, bool debugMode) {
+
+  incomingByte = incomingSerial;
+  incomingString += incomingByte;
+  if (incomingByte == ',') {
+    int str_len = incomingString.length() - 1;
+    incomingCSV[currentCSVIndex] = incomingString.substring(0, str_len);
+    if (debugMode == 1) {
+      ser1.print("Incoming ");
+      ser1.print(currentCSVIndex);
+      ser1.print(" = ");
+      ser1.println(incomingCSV[currentCSVIndex].toFloat() * 90.0);
+    }
+    currentCSVIndex++;
+    incomingString = "";
+  }
+  if (incomingByte == '\n') {
+    int str_len = incomingString.length() - 1;
+    incomingCSV[currentCSVIndex] = incomingString.substring(0, str_len);
+    maxCSVIndex = currentCSVIndex;
+    if (debugMode == 1) {
+      ser1.print("Incoming ");
+      ser1.print(currentCSVIndex);
+      ser1.print(" = ");
+      ser1.println(incomingCSV[currentCSVIndex].toFloat() * 90.0);
+      ser1.print("max Index  = ");
+      ser1.println(maxCSVIndex);
+      ser1.println();
+    }
+    dataPreparation();
+    incomingString = "";
+    currentCSVIndex = 0;
+  }
 }
